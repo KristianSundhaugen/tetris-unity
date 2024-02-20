e626da3b7ddcf6dded88bc3ccf744d053e7121f8
KristianSundhaugen <kristiansundhaugen@gmail.com>
2024-02-20 22:52:34 +0100

    Added functionality for moving to the left (A), right (D), down (S) and hard down (Space). Also added posibility of rotation using key E or Q

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -7,6 +7,16 @@ public class Board : MonoBehaviour
     public Piece activePiece { get; private set; }
     public TetrominoData[] tetrominos;
     public Vector3Int spawnPosition;
+    public Vector2Int boardSize = new Vector2Int(10, 20);
+
+    public RectInt Bounds
+    {
+        get
+        {
+            Vector2Int position = new Vector2Int(-this.boardSize.x / 2, -this.boardSize.y / 2);
+            return new RectInt(position, this.boardSize);
+        }
+    }
 
     private void Awake()
     {
@@ -40,5 +50,39 @@ public class Board : MonoBehaviour
             Vector3Int tilePosition = piece.cells[i] + piece.position;
             this.tilemap.SetTile(tilePosition, piece.data.tile);
         }
+    }
+
+    public void Clear(Piece piece)
+    {
+        for (int i = 0; i < piece.cells.Length; i++)
+        {
+            Vector3Int tilePosition = piece.cells[i] + piece.position;
+            this.tilemap.SetTile(tilePosition, null);
+        }
+    }
+
+
+    public bool IsValidPosition(Piece piece, Vector3Int position)
+    {
+        RectInt bounds = this.Bounds;
+
+        for (int i = 0; i < piece.cells.Length; i++)
+        {
+            Vector3Int tilePosition = piece.cells[i] + position;
+
+            //Checks if a tile position would be out of bounds 
+            if (!bounds.Contains((Vector2Int)tilePosition))
+            {
+                return false;
+            }
+
+            //Checks if the tilemap already has a tile that occupies that space
+            if (this.tilemap.HasTile(tilePosition))
+            {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
